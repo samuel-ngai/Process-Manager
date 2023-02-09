448bd7611834087e4dcc2e1bd9e1ea0a64bf5ead
Samuel Ngai <samuelngai@Samuels-MBP.hitronhub.home>
2023-02-08 21:29:19 -0800

    why did she leave

--- p1s/PMan.c
+++ p1s/PMan.c
@@ -233,7 +233,7 @@ char* readStatFile(char* procPath) {
        printf("Failed to access file\n");
    } 
    int i = 0;
-   while((read = getline(&line, 0, statFile)) != 0) {
+   while((read = getline(&line, 0, statFile)) != -1) {
        if(i == 0) {
            printf("comm: %s\n", line);
        }
@@ -242,6 +242,7 @@ char* readStatFile(char* procPath) {
        }
        if(i == 24) {
            printf("rss: %s\n", line);
+           break;
        }
        i++;
    }
